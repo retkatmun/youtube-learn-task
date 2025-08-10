47bb9b8867b3a142cc6ea60d597fd2474a75b0df
Retkatmun <retkatmun1@gmail.com>
2025-08-10 08:20:48 +0000

    first commit

---
+++ class1.py
@@ -0,0 +1,42 @@
+#task1
+'''
+full_name = input("Enter your full name")
+
+print(full_name.upper())
+print(full_name.lower())
+
+length = len(full_name)
+print(length)
+'''
+
+#task2
+'''
+sentence = input("Enter a sentence")
+search = input("search for word")
+
+find = sentence.find(search)
+print(find)
+'''
+
+#task3
+'''
+sentence = input("Enter a sentence")
+ask_count =input("Enter word/aphabet to count")
+
+count = sentence.count(ask_count)
+print(count)
+
+'''
+
+#task4
+
+sentence = input("Enter a sentence")
+ask_replace =input("word to replace")
+replace_ask = input("relace word")
+
+replace = sentence.replace(ask_replace, replace_ask)
+print(sentence)
+print(f"{ask_replace} {replace}")
+
+#task5
+
