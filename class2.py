89e980b46f79f24acfd8ef6a08cca6e84e471c03
Retkatmun <retkatmun1@gmail.com>
2025-08-10 14:57:23 +0000

    set

--- class2.py
+++ class2.py
@@ -1,15 +1,29 @@
-#task1
+#task1 list
+'''
 favorite_food = ['beans','pasta','rice','egusi','vegetable soup']
 print(favorite_food)
 favorite_food.append('yam')
 favorite_food.remove('pasta')
 print(favorite_food)
 print(favorite_food[::-1])
+'''
 
-#task2
+#task2 tuples
+'''
 numbers = (1,2,3,4)
 print(numbers[0])
 print(numbers[-1])
 
 mix = numbers + (100,200)
 print(mix)
+'''
+
+#task3 set
+fruits = {'orange','banana','kiwi','apple','pineapple'}
+print(fruits)
+fruits.add('gwava')
+print(fruits)
+fruits.remove('banana')
+print(fruits)
+print('apple' in fruits )
+
