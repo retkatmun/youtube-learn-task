a0d143aad3e8db44058baa604d5acfa727114137
Retkatmun <retkatmun1@gmail.com>
2025-08-10 14:34:17 +0000

    tuples

---
+++ class2.py
@@ -0,0 +1,7 @@
+#task1
+favorite_food = ['beans','pasta','rice','egusi','vegetable soup']
+print(favorite_food)
+favorite_food.append('yam')
+favorite_food.remove('pasta')
+print(favorite_food)
+print(favorite_food[::-1])
