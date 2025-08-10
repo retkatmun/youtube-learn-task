69815b384b778b6cd4ef5e4fd0df0817ef7ace5e
Retkatmun <retkatmun1@gmail.com>
2025-08-10 18:04:43 +0000

    print key value

--- class3.py
+++ class3.py
@@ -8,3 +8,22 @@ print(student["name"])
 student["hubby"] = "coding"
 student["grade"] = "B"
 print(student)
+
+convert = list(student)
+print(type(convert))
+
+third = convert[2]
+print(third)
+
+for key in student:
+	print(key)
+for value in student:
+	print(value)
+
+student.pop("hubby")
+print(student)
+
+for key, value in student.items():
+	print(f"{key}: {value}")
+
+	
